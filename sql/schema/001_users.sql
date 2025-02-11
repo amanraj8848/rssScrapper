6b2d1fdc909d6589504152aca49beaca7906385a
amanraj8848 <aman.delhi7618@gmail.com>
2025-02-12 02:55:05 +0530

    Postgres DB connection

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,11 @@
+-- +goose Up
+CREATE TABLE users (
+    if UUID PRIMARY KEY,    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    name TEXT NOT NULL
+);
+
+
+-- -goose Down
+DROP TABLE users;
